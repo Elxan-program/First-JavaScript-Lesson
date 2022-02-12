0416ca9177a31ec10fb97eb5bea9401c6c9ea3f0
Elxan-program <82094509+Elxan-program@users.noreply.github.com>
2022-02-12 16:18:13 +0400

    Ho capito

---
+++ First Javascript Lesson/index.js
@@ -0,0 +1,50 @@
+// var myimg= document.getElementById("myimg");
+// var myheader= document.getElementById("myheader");
+
+// function TurnOn(){
+//     myimg.src="Images/pic_bulbon.png";
+//     myimg.style.display="inline-block"
+//     myheader.style.fontSize ="5em";
+// }
+
+// function TurnOff(){
+//     myimg.src="Images/pic_bulboff.png";
+//     myimg.style.display="none"
+//     myheader.style.fontSize ="1em";
+// }
+
+// function Start(){
+//     // var allPage = document.documentElement.innerHTML;
+
+//     // alert(allPage);
+
+//     // var element = document.getElementById("Demo");
+//     // element.innerHTML="Hello javaScript";
+
+//     // window.alert("Wanna Sign In?");
+
+//     // document.write("Wanna Sign In?");
+
+//     // console.log("Button clicked Successfully");
+
+
+// }
+
+function main(){
+    // let x = 100, y = 200;
+    // // alert(x + y);
+    // header = document.getElementById("Demo");
+    // header.innerHTML=x*y;
+
+    // let Gtskdr= 10;
+    // let mthrfckr= 20;
+    // alert(Gtskdr + mthrfckr);
+
+    let name = prompt("Enter Your Name");
+    let age = prompt("Enter Your Age");
+}
+alert("0123456789876543210");
+main();
+
+
+
